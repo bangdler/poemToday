cb1e52e6d9d3b71f22087073ccb8bd12a2cf4380
bangdler <zbthz90@gmail.com>
2022-10-28 04:52:08 +0900

    feat: logo 컴포넌트 및 접근성을 위한 blind text 컴포넌트 구현

---
+++ FE/src/components/Logo.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+import BlindText from '@/components/BlindText';
+
+export default function Logo({ name, link }) {
+  return (
+    <S_Logo>
+      <Link to={link}>
+        {name}
+        <BlindText text={'홈으로 이동합니다.'} />
+      </Link>
+    </S_Logo>
+  );
+}
+
+const S_Logo = styled.h1`
+  font-size: 2.6rem;
+`;
