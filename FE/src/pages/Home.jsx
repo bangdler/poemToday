6a72089a242a7aca619d92808b3446eb5935bb05
bangdler <zbthz90@gmail.com>
2022-11-03 21:39:56 +0900

    feat: json-server 설치

--- FE/src/pages/Home.jsx
+++ FE/src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import styled from 'styled-components';
 
 import Header from '@/components/Header';
 
@@ -6,6 +7,17 @@ export default function Home() {
   return (
     <>
       <Header />
+      <S_Div></S_Div>
+      <S_Div2></S_Div2>
     </>
   );
 }
+
+const S_Div = styled.div`
+  height: 1000px;
+`;
+
+const S_Div2 = styled.div`
+  background-color: #ffd700;
+  height: 1000px;
+`;
