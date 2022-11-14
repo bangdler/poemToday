495940c12e211ecb8a6ba939580bfa04a615b59d
bangdler <zbthz90@gmail.com>
2022-11-14 21:47:21 +0900

    feat: inputBox 컴포넌트 구현

--- FE/src/components/common/InputBox.jsx
+++ FE/src/components/common/InputBox.jsx
@@ -1,42 +1,35 @@
 import React from 'react';
 import styled from 'styled-components';
 
-export default function InputBox({ title, onChange, value, type = 'text', verification = false, errorNote = '' }) {
+import palette from '@/style/palette';
+
+export default function InputBox({ title, onChange, value, type = 'text' }) {
   return (
     <S_Wrapper>
       <S_Title>{title}</S_Title>
-      <S_Input type={type} onChange={onChange} value={value} />
-      <S_Span>{!verification && errorNote}</S_Span>
+      <S_Input type={type} onChange={onChange} value={value} autoComplete="new-password" />
     </S_Wrapper>
   );
 }
 
 const S_Wrapper = styled.div`
   ${({ theme }) => theme.mixin.flexBox({ direction: 'column' })}
-  height: 80px;
-  margin: 10px 0;
-  width: 90%;
+  width: 100%;
 `;
 
 const S_Title = styled.h2`
   width: 100%;
   font-size: 1.6rem;
   color: ${({ theme }) => theme.mode.textColor};
-  margin-bottom: 0.5rem;
+  margin-bottom: 1rem;
 `;
 
 const S_Input = styled.input`
-  box-sizing: border-box;
   width: 100%;
   border-bottom: ${({ theme }) => theme.mode.borderColor};
   font-size: 2rem;
   color: ${({ theme }) => theme.mode.textColor};
-  margin-bottom: 0.5rem;
-`;
-
-const S_Span = styled.span`
-  width: 100%;
-  color: red;
-  font-size: 1rem;
-  height: 20px;
+  &:focus {
+    border-bottom: 1px solid ${palette.gray[7]};
+  }
 `;
