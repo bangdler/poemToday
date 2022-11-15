0d6f1c10bbbd9ebe42b6200cf42a9b3c4ce52cf9
bangdler <zbthz90@gmail.com>
2022-11-16 02:25:35 +0900

    feat: 로그인 회원가입 관련 상태 관리를 위한 AuthProvider 구현, api 서버 요청을 위한 로직 useSumbitAuth hook으로 분리

--- FE/src/context/AuthProvider.js
+++ FE/src/context/AuthProvider.js
@@ -1,9 +1,11 @@
-import React, { createContext, useReducer, useMemo, useCallback } from 'react';
+import React, { createContext, useReducer, useMemo, useCallback, useContext, useState } from 'react';
+
+import * as api from '@/api/auth.js';
 
 export const AuthContext = createContext();
 export const AuthDispatchContext = createContext();
 
-const InitialAuthForm = {
+const initialAuthForm = {
   register: {
     username: '',
     password: '',
@@ -13,31 +15,37 @@ const InitialAuthForm = {
     username: '',
     password: '',
   },
+  authResponse: null,
+  authError: null,
 };
 
-const reducer = (state, action) => {
+const authFormReducer = (state, action) => {
   switch (action.type) {
     case 'INITIALIZE':
-      return { ...state, [action.field]: InitialAuthForm[action.field] };
+      return { ...state, [action.field]: initialAuthForm[action.field], authError: null, authResponse: null };
     case 'CHANGE':
       return { ...state, [action.field]: { ...state[action.field], [action.key]: action.value } };
+    case 'AUTH_SUCCESS':
+      return { ...state, authError: null, authResponse: action.response };
+    case 'AUTH_FAILURE':
+      return { ...state, authError: action.error };
     default:
       return state;
   }
 };
 
 export default function AuthProvider({ children }) {
-  const [authForm, dispatch] = useReducer(reducer, InitialAuthForm);
+  const [authForm, authFormDispatch] = useReducer(authFormReducer, initialAuthForm);
 
   const initializeForm = useCallback(({ field }) => {
-    dispatch({
+    authFormDispatch({
       type: 'INITIALIZE',
       field,
     });
   }, []);
 
   const changeForm = useCallback(({ field, key, value }) => {
-    dispatch({
+    authFormDispatch({
       type: 'CHANGE',
       field,
       key,
@@ -45,7 +53,24 @@ export default function AuthProvider({ children }) {
     });
   }, []);
 
-  const memoizedAuthDispatches = useMemo(() => ({ initializeForm, changeForm }), [initializeForm, changeForm]);
+  const authSuccess = useCallback(({ response }) => {
+    authFormDispatch({
+      type: 'AUTH_SUCCESS',
+      response,
+    });
+  }, []);
+
+  const authFail = useCallback(({ error }) => {
+    authFormDispatch({
+      type: 'AUTH_FAILURE',
+      error,
+    });
+  }, []);
+
+  const memoizedAuthDispatches = useMemo(
+    () => ({ initializeForm, changeForm, authSuccess, authFail }),
+    [initializeForm, changeForm, authSuccess, authFail]
+  );
 
   return (
     <AuthContext.Provider value={authForm}>
@@ -53,3 +78,25 @@ export default function AuthProvider({ children }) {
     </AuthContext.Provider>
   );
 }
+
+export const useSubmitAuth = () => {
+  const authForm = useContext(AuthContext);
+  const { authSuccess, authFail } = useContext(AuthDispatchContext);
+  const [authLoading, setAuthLoading] = useState({ login: false, register: false });
+
+  const submitAuth = async ({ type }) => {
+    const loadingStart = { ...authLoading, [type]: true };
+    setAuthLoading(loadingStart);
+
+    try {
+      const response = await api[type]({ username: authForm[type].username, password: authForm[type].password });
+      authSuccess({ response: response.data });
+    } catch (e) {
+      authFail({ error: e });
+    }
+    const loadingFinish = { ...authLoading, [type]: false };
+    setAuthLoading(loadingFinish);
+  };
+
+  return [authLoading, submitAuth];
+};
