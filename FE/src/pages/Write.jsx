16c1bd12516d27204691897e154162995ef9222e
bangdler <zbthz90@gmail.com>
2022-11-23 01:12:17 +0900

    feat: write page 접속 시 로그인 확인 반영

--- FE/src/pages/Write.jsx
+++ FE/src/pages/Write.jsx
@@ -1,20 +1,39 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 
 import Editor from '@/components/Editor';
 import Header from '@/components/Header';
+import WriteActionButtons from '@/components/WriteActionButtons';
+import { AuthContext, useAuth } from '@/context/AuthProvider';
 
 export default function Write() {
+  const authForm = useContext(AuthContext);
+  const { checkUser } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    checkUser();
+  }, []);
+
+  useEffect(() => {
+    if (authForm.userError) {
+      alert('로그인이 필요합니다.');
+      navigate('/login');
+    }
+  }, [authForm.userError]);
+
   return (
     <>
       <Header />
-      <PageLayout>
+      <S_FormLayout>
         <Editor />
-      </PageLayout>
+        <WriteActionButtons />
+      </S_FormLayout>
     </>
   );
 }
 
-const PageLayout = styled.div`
+const S_FormLayout = styled.form`
   padding: 1rem;
 `;
