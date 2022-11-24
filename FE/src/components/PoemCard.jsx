e0ac9aad5341113ca089649287e0fbb5fde0cad9
bangdler <zbthz90@gmail.com>
2022-11-24 15:40:40 +0900

    feat: PoemCard 컴포넌트 UI 구현

---
+++ FE/src/components/PoemCard.jsx
@@ -0,0 +1,77 @@
+import React, { useContext, useEffect } from 'react';
+import ReactQuill from 'react-quill';
+import styled from 'styled-components';
+
+export default function PoemCard({ id, title, body, category }) {
+  return (
+    <S_Wrapper>
+      <S_Title>{title}</S_Title>
+      <S_Line />
+      <S_CategoryContainer>
+        {category.map((it, idx) => {
+          if (it.checked) {
+            return <S_CategoryTag key={idx}>#{it.name}</S_CategoryTag>;
+          }
+        })}
+      </S_CategoryContainer>
+      <S_Line />
+      <S_ReactQuill readOnly={true} value={body} modules={{ toolbar: null }} />
+    </S_Wrapper>
+  );
+}
+
+const S_Wrapper = styled.div`
+  width: 260px;
+  min-height: 330px;
+  background-color: ${({ theme }) => theme.mode.cardColor};
+  box-shadow: 4px 12px 30px 6px rgb(0 0 0 / 9%);
+  border-radius: 5px;
+  padding: 20px 10px;
+  cursor: pointer;
+  &:hover {
+    transform: translateY(-10px);
+  }
+`;
+
+const S_Title = styled.h2`
+  min-height: 40px;
+  font-size: 3.4rem;
+  width: 100%;
+  text-align: center;
+  ${({ theme }) => theme.mixin.flexBox({})};
+  ${({ theme }) => theme.mixin.ellipsis({ lineNum: 2, lineHeight: '3.4rem' })};
+  user-select: none;
+`;
+
+const S_Line = styled.div`
+  border: 2px solid ${({ theme }) => theme.mode.borderColor};
+  margin: 10px 0;
+`;
+const S_CategoryContainer = styled.div`
+  width: 100%;
+  ${({ theme }) => theme.mixin.flexBox({})}
+  > * {
+    margin-right: 6px;
+  }
+`;
+
+const S_CategoryTag = styled.span`
+  font-size: 1.6rem;
+`;
+
+const S_ReactQuill = styled(ReactQuill)`
+  .ql-editor {
+    padding: 0.5rem 1rem;
+    height: 200px;
+    overflow-y: hidden;
+    > * {
+      word-break: keep-all;
+      word-wrap: break-word;
+    }
+  }
+  .ql-container {
+    border: none;
+    font-family: 'Gamja Flower';
+    font-size: 1.6rem;
+  }
+`;
