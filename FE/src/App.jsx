ada6f0bf71490e0eae4f438e823cce705361bf3a
bangdler <zbthz90@gmail.com>
2023-01-13 19:23:38 +0900

    feat: react-helmet-async 설치 및 Meta 컴포넌트 구현

--- FE/src/App.jsx
+++ FE/src/App.jsx
@@ -1,31 +1,24 @@
 import React from 'react';
+import { HelmetProvider } from 'react-helmet-async';
 
-import AuthProvider from '@/context/AuthProvider';
-import LoadingProvider from '@/context/LoadingProvider';
-import PoemListProvider from '@/context/PoemListProvider';
-import PoemProvider from '@/context/PoemProvider';
+import BaseMeta from '@/components/metaComponents/BaseMeta';
+import ContextsProvider from '@/context/ContextsProvider';
 import { ThemeProvider } from '@/context/ThemeProvider';
-import UserProvider from '@/context/UserProvider';
 import Router from '@/Router';
 import { GlobalStyle } from '@/style/global';
 
 function App() {
   return (
     <div className="app">
-      <ThemeProvider>
-        <LoadingProvider>
-          <UserProvider>
-            <AuthProvider>
-              <PoemListProvider>
-                <PoemProvider>
-                  <GlobalStyle />
-                  <Router />
-                </PoemProvider>
-              </PoemListProvider>
-            </AuthProvider>
-          </UserProvider>
-        </LoadingProvider>
-      </ThemeProvider>
+      <HelmetProvider>
+        <ThemeProvider>
+          <ContextsProvider>
+            <BaseMeta />
+            <GlobalStyle />
+            <Router />
+          </ContextsProvider>
+        </ThemeProvider>
+      </HelmetProvider>
     </div>
   );
 }
