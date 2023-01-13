c2c84af6771aa2e0ea98e874ab092ae70101447c
bangdler <zbthz90@gmail.com>
2023-01-14 01:13:36 +0900

    fix: react-helmet 이용할 경우 meta og 를 인식하지 못하여 meta tag 를 직접 index.html 에 삽입

--- FE/src/App.jsx
+++ FE/src/App.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
-import { HelmetProvider } from 'react-helmet-async';
 
-import BaseMeta from '@/components/metaComponents/BaseMeta';
 import ContextsProvider from '@/context/ContextsProvider';
 import { ThemeProvider } from '@/context/ThemeProvider';
 import Router from '@/Router';
@@ -9,17 +7,14 @@ import { GlobalStyle } from '@/style/global';
 
 function App() {
   return (
-    <HelmetProvider>
-      <div className="app">
-        <ThemeProvider>
-          <ContextsProvider>
-            <BaseMeta />
-            <GlobalStyle />
-            <Router />
-          </ContextsProvider>
-        </ThemeProvider>
-      </div>
-    </HelmetProvider>
+    <div className="app">
+      <ThemeProvider>
+        <ContextsProvider>
+          <GlobalStyle />
+          <Router />
+        </ContextsProvider>
+      </ThemeProvider>
+    </div>
   );
 }
 
